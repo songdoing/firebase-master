65a46253f9d2143ee07994336861b60972825de5
wonjinsong <msmk22239@gmail.com>
2020-06-11 14:15:30 -0400

    google auth

---
+++ public/fireinit.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBRIBmU64B8kV5-Y6erT1uR4LTtlgu7Pk8",
+    authDomain: "jenny-master.firebaseapp.com",
+    databaseURL: "https://jenny-master.firebaseio.com",
+    projectId: "jenny-master",
+    storageBucket: "jenny-master.appspot.com",
+    messagingSenderId: "321622422493",
+    appId: "1:321622422493:web:ce33a1751abc2dc6877c34"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
